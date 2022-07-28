3c8da731e63b9a7bfd39a46cb1be19d6c4db622b
Timurgess <timurgess@yandex.ru>
2022-07-28 16:15:03 +0300

    Задание_1

---
+++ HWW8/Задание_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+Console.WriteLine("Эта программа покажет все натуральные числа от M до N.");
+Console.WriteLine("Задайте значение M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значение N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+void FindNumber(int  m, int n)
+{
+    for(int i = m; i <= n; i++)
+    {
+        Console.Write(i + ", ");
+    }
+}
+FindNumber(numberM, numberN);
