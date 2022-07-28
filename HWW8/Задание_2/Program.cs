d612daaf6a96fadde3e623ac81a5619d88b0e7b9
Timurgess <timurgess@yandex.ru>
2022-07-28 16:17:23 +0300

    Задание_2

---
+++ HWW8/Задание_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Эта программа покажет сумму натуральных чисел от M до N.");
+Console.WriteLine("Задайте значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = (n*(n + 1))/2 - ((m-1)*((m-1) + 1))/2;
+Console.WriteLine("Сумма натуральных чисел от " + m + " до " + n + " -> " + sum);
